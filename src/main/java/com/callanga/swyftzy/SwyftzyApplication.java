b09d4cddc85ecfbe77d2c6735ae565d258160e23
pogi <aaroncallanga01@gmail.com>
2026-05-27 17:08:13 +0800

    set-up: initialization

---
+++ src/main/java/com/callanga/swyftzy/SwyftzyApplication.java
@@ -0,0 +1,13 @@
+package com.callanga.swyftzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwyftzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwyftzyApplication.class, args);
+	}
+
+}
